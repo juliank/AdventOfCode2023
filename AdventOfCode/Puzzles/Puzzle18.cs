ad340c759d582c3a2e08639e54f884b6a79b3a12
Julian Klock <julian.klock@gmail.com>
2023-12-18 07:56:20 +0100

    Rename puzzles, create dig plan

--- AdventOfCode/Puzzles/Puzzle18.cs
+++ AdventOfCode/Puzzles/Puzzle18.cs
@@ -1,16 +1,17 @@
 namespace AdventOfCode.Puzzles;
 
-public class Puzzle00 : Puzzle<long, long>
+public class Puzzle18 : Puzzle<string, long>
 {
-    private const int PuzzleId = 00;
+    private const int PuzzleId = 18;
 
-    public Puzzle00() : base(PuzzleId) { }
+    public Puzzle18() : base(PuzzleId) { }
 
-    public Puzzle00(params long[] inputEntries) : base(PuzzleId, inputEntries) { }
+    public Puzzle18(params string[] inputEntries) : base(PuzzleId, inputEntries) { }
 
     public override long SolvePart1()
     {
-        throw new NotImplementedException();
+        var digPlan = CreateDigPlan();
+        return -1;
     }
 
     public override long SolvePart2()
@@ -18,8 +19,38 @@ public class Puzzle00 : Puzzle<long, long>
         throw new NotImplementedException();
     }
 
-    protected internal override IEnumerable<long> ParseInput(string inputItem)
+    private List<Point<string>> CreateDigPlan()
     {
-        yield return 0;
+        var digPlan = new List<Point<string>>();
+
+        var currentPoint = new Point(0, 0);
+        foreach (var item in InputEntries)
+        {
+            var parts = item.Split(' ');
+
+            var direction = parts[0] switch
+            {
+                "U" => Direction.N,
+                "D" => Direction.S,
+                "R" => Direction.E,
+                "L" => Direction.W,
+                _ => throw new ArgumentException($"Unknown direction {parts[0]}")
+            };
+            var steps = int.Parse(parts[1]);
+            var color = parts[2][1..^1];
+
+            for (var i = 0; i < steps; i++)
+            {
+                currentPoint = currentPoint.Get(direction)!; // No max/min, so we'll always get a next point
+                var digPoint = new Point<string>(currentPoint.X, currentPoint.Y) { Value = color };
+                digPlan.Add(digPoint);
+            }
+        }
+        return digPlan;
+    }
+
+    protected internal override IEnumerable<string> ParseInput(string inputItem)
+    {
+        yield return inputItem;
     }
 }
