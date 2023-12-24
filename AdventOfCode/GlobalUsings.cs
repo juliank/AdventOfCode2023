f937f51fd75e11a746d870cea77343c7414145ce
Julian Klock <julian.klock@gmail.com>
2023-12-24 08:52:12 +0100

    Change target framework to net8.0-windows with UseWPF
    This is to allow using types such as Vector and Point
    in System.Windows, which might be usefor for performing
    vector operations and calculations.

--- AdventOfCode/GlobalUsings.cs
+++ AdventOfCode/GlobalUsings.cs
@@ -2,6 +2,7 @@ global using AdventOfCode.Models;
 global using AdventOfCode.Puzzles;
 global using AdventOfCode.Utils;
 global using System.Diagnostics;
+global using System.IO;
 global using System.Numerics;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
